32305800d715c67b1d661a6a0a16476bf63c2b41
katiepell42 <57158239+katiepell42@users.noreply.github.com>
2021-03-31 20:55:35 -0700

    food, user classes
    Create classes Food and User, with variables and methods. Not sure how to do the "useFood" thing - delete it from database?

---
+++ Food.py
@@ -0,0 +1,58 @@
+import datetime
+
+class Food:
+    def __init__(self, inKey, inName, inExpired, inExpDate, inQuantity, inType):
+        self.surrogateKey = inKey
+        self.name = inName
+        self.expired = inExpired
+        self.expDate = inExpDate
+        self.quantity = inQuantity
+        self.type = inType
+
+
+    def setSurrogate(self, inKey):
+        self.surrogateKey = inKey
+
+    def getSurrogate(self):
+        return self.surrogateKey
+
+    def setName(self, inName):
+        self.name = inName
+
+    def getName(self):
+        return self.name
+
+    def setExpired(self, input):
+        self.expired = input
+
+    def getExpired(self):
+        return self.expired
+
+    def setType(self, inType):
+        self.type = inType
+
+    def getType(self):
+        return self.type
+
+    def setExpDate(self, inDate):
+        self.expDate = inDate
+
+    def getExpDate(self):
+        return self.expDate
+
+
+    
+
+
+# Create an object of type Food (with initialization values)
+myFood = Food(1, "Katie", True, datetime.datetime(2021, 3, 29), 10, "produce")
+
+
+print(myFood.surrogateKey)
+
+print("***************")
+
+print(myFood.getSurrogate())
+
+
+
---
+++ User.py
@@ -0,0 +1,24 @@
+class User:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def getUsername(self):
+        return self.username
+
+    def setUsername(self, name):
+        self.name = name
+    
+    def getEmail(self):
+        return self.email
+
+    def setEmail(self, email):
+        self.email = email
+
+    def getPassword(self):
+        return self.password
+
+    def setPassword(self, password):
+        self.password = password
+
