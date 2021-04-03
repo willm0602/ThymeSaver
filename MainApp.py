eaf4eace02c48f15ae1801975aecee1b87311a0f
Troy <TroyAustynWilson@gmail.com>
2021-04-02 18:25:54 -0700

    Added checklist, button, and cleaned up some icons

--- MainApp.py
+++ MainApp.py
@@ -1,7 +1,7 @@
-#test for commit
 from Food import Food
 from User import User
 
+from kivymd.uix.card import MDCardSwipe
 
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
@@ -38,7 +38,7 @@ KV = '''
         text_color: root.text_color
 
 
-<ContentNavigationDrawer>:
+<ContentNavigationDrawer>: #All of the info in the nav drawer
     orientation: "vertical"
     padding: "8dp"
     spacing: "8dp"
@@ -48,33 +48,82 @@ KV = '''
         size_hint_y: None
         height: avatar.height
 
-        Image:
+        Image: #User image, not necessary but could be cool. Current Captain Wheeze
             id: avatar
             size_hint: None, None
             size: "56dp", "56dp"
             source: "CaptainWheezeopp.png"
 
-    MDLabel:
+    MDLabel: # This will eventually be the username of the user
         text: "A$AP Sidebar"
         font_style: "Button"
         size_hint_y: None
         height: self.texture_size[1]
 
-    MDLabel:
+    MDLabel: # and this will be the users email
         text: "SadWill@gmail.com"
         font_style: "Caption"
         size_hint_y: None
         height: self.texture_size[1]
 
-    ScrollView:
-
+    ScrollView: #This is the nav list in the navbar, probably doesn't need a scrollview
         DrawerList:
             id: md_list
-Screen:
-    MDList:
-        id:container
 
-    NavigationLayout:
+<ListItemWithCheckbox>:
+    IconLeftWidget:
+        icon: root.icon
+    RightCheckbox:
+
+<SwipeItem>:
+
+    size_hint_y: 1
+    height: content.height
+    pos_hint: {"center_x": 0.5, "center_y": 0.5}
+
+    MDCardSwipeLayerBox:
+        padding: "8dp"
+
+        MDIconButton:
+            icon: "trash-can"
+            pos_hint: {"center_y": 0.5}
+            on_release: app.remove_item(root)
+
+        MDIconButton:
+            icon: "database-plus"
+            pos_hint: {"center_y": 0.5}
+            on_release: app.remove_item(root)
+            
+
+    MDCardSwipeFrontBox:
+        OneLineListItem: # was OneLineListItem
+            id: content
+            text: root.text
+            icon: root.icon
+            _no_ripple_effect: True
+
+        RightCheckBox:
+
+MDScreen: #main screen
+    MDBoxLayout:
+        orientation: "vertical"
+        spacing: "10dp"
+
+    ScrollView:
+        scroll_timeout: 100
+
+        MDList:
+            id:md_list
+            padding: 0
+
+    MDFloatingActionButtonSpeedDial:
+        data: app.data
+        callback: app.callback #testing here
+        root_button_anim: True
+        hint_animation: True
+
+    
+    NavigationLayout: # This is the top bar for naviagation, currently blue
 
         ScreenManager:
 
@@ -91,29 +140,39 @@ Screen:
                     Widget:
 
 
-        MDNavigationDrawer:
+        MDNavigationDrawer: # This is the ||| button on the top left that makes the nav bar work
             id: nav_drawer
 
-            ContentNavigationDrawer:
+            ContentNavigationDrawer: # This is the contents that live inside it
                 id: content_drawer
 '''
 
 user = User("PeterParker","spidyman@gmail.com","password")
 
 
+class SwipeItem(MDCardSwipe):
+    '''' Card with behavior '''
+    text = StringProperty()
+    icon = StringProperty('android')
+
+class ListItemWithCheckbox(OneLineAvatarIconListItem):
+    '''' list item '''
+    icon = StringProperty('android')
+
 class ContentNavigationDrawer(BoxLayout):
     pass
 
+class RightCheckBox(IRightBodyTouch,MDCheckbox):
+    '''' right container '''
 
-class ItemDrawer(OneLineIconListItem):
+class ItemDrawer(OneLineIconListItem): # icons and names in the nav list
     icon = StringProperty()
-    text_color = ListProperty((0, 0, 0, 1))
+    text_color = ListProperty((0, 0, 0, 1)) #colors gets set back to black after click?
 
 
 class DrawerList(ThemableBehavior, MDList):
     def set_color_item(self, instance_item):
         """Called when tap on a menu item."""
-
         # Set the color of the icon and text for the menu item.
         for item in self.children:
             if item.text_color == self.theme_cls.primary_color:
@@ -123,23 +182,65 @@ class DrawerList(ThemableBehavior, MDList):
 
 
 class TestNavigationDrawer(MDApp):
+    instance = "database"
+    
+    data = {
+        'emoticon-angry': "WILL, I'LL END YOUR BLOODLINE",
+        'database-plus': 'Add all checked to Pantry',
+        'delete': 'Delete all checked',
+        'plus':'Add item to list',
+    }
+
+    def callback(self,instance): #when you press on one of the items in the circle button it prints the name of the icon being used
+        print(instance.icon)
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen = Builder.load_string(KV)
+
     def build(self):
-        return Builder.load_string(KV)
+        return self.screen
+
+    def remove_item(self,instance):
+        self.screen.ids.md_list.remove_widget(instance)
+
 
     def on_start(self):
-
-        for i in range(10):
-            image = IconLeftWidget(icon="CaptainWheezeopp.png")
-            items = OneLineAvatarIconListItem(text = user.username + str(i))
-            items.add_widget(image)
-            self.root.ids.container.add_widget(items)
-
         icons_item = {
-            "folder": "Food",
-            "account-multiple": "Recipes",
-            "star": "Pantry",
+            "food-apple": "Food",
+            "pasta": "Recipes",
+            "database": "Pantry",
+            "logout": "Log out"
         }
-        
+
+        itemsList = [
+            "", #empty item because the positioning puts it under the nav bar
+            "pizza",
+            "Banana",
+            "Lamb",
+            "Chicky nuggies",
+            "Gogurt",
+            "Cheeze stick",
+            "Cold pasta sauce",
+            "Spam",
+            "Paimon",
+            "Shallot",
+            "Carrot",
+            "Way too many green onions",
+            "Chili",
+            "Paprika",
+            "Flour",
+            "Sugar",
+            "Apples",
+            "Sour patch kids"
+        ]
+
+        for i in itemsList:
+            self.screen.ids.md_list.add_widget(
+                SwipeItem(text = i, icon = "pasta")
+            )
+
         for icon_name in icons_item.keys():
             self.root.ids.content_drawer.ids.md_list.add_widget(
                 ItemDrawer(icon=icon_name, text=icons_item[icon_name])
