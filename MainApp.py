78fa5cf58a1e336c42dc734da09d288760be1270
Troy A Wilson <38123313+TroyAWilson@users.noreply.github.com>
2021-05-11 13:02:50 -0700

    Update to Pantry Screen
    The pantry screen has its basics implemented: the list loads from based on the items a user has in the database
    I've also removed some of the things that you wouldn't need while interacting with the Pantry, primarily things like "add to pantry"
    I also changed the name of the .kv file to something that is more descriptive

--- MainApp.py
+++ MainApp.py
@@ -45,9 +45,6 @@ class Registration(Screen): # Registration Window
         DB = DBHandler()
         if self.valid(username,DB):
             DB.exec(query)
-            return('window2')
-        self.errorDialog()
-        return('registration')
 
     def valid(self, username, db):
         users = db.exec(f"SELECT * FROM User WHERE username = '{username}'")
@@ -70,10 +67,8 @@ class Login(Screen): # Login Window
             email = self.getEmail(username, db)
             user = User(username, email, password)
             App.user = user
-            return('window2')
         else:
             self.errorDialog()
-            return('login')
 
     def errorDialog(self):
         self.dialog = MDDialog(
@@ -98,8 +93,207 @@ class Login(Screen): # Login Window
 class Recipes(Screen): # Recipe Window
     pass
 
-class Pantry(Screen): # Pantry Window
-    pass
+class Pantry(Screen): # Pantry Screen
+    #class variable definitions
+    data = {
+        # 'database-plus': 'Add all checked to Pantry',
+        'delete': 'Delete all checked from Pantry',
+        'plus':'Add item to Pantry',
+    }
+    
+    bufferDate = None
+    container = ObjectProperty()
+    quantity = ObjectProperty()
+    alreadyCheck = False
+    alreadyCheckNav = False
+
+    PantryList = [ #to be filled by database
+            "", #empty item because the positioning puts it under the nav bar
+        ]
+    
+    def on_enter(self):
+        icons_item = { #This needs extra items at the bottom to fill out the nav bar - mid priority bug to be fixed later
+            "food-apple": "Food",
+            "pasta": "Recipes",
+            "database": "Pantry",
+            "brush": "Theme", #completely unesccesary but would be cool to customize colors of the app
+            #see MDThemePicker https://kivymd.readthedocs.io/en/latest/components/pickers/index.html
+            "logout": "log out",
+            "a":"",
+            "b":"",
+            "c":"",
+            "d":"",
+        }
+
+        #pull items owned by a user from the database
+        PantryItems = App.db.exec(f"SELECT name FROM Food WHERE owner = '{App.user.username}' ")
+
+        #fill the list with them
+        for item in PantryItems:
+            self.PantryList.append(item[0])
+
+        print(PantryItems)#debug
+
+        if self.alreadyCheckNav == False: #If the navbar is already full, don't fill it again
+            for icon_name in icons_item.keys():
+                self.ids.content_drawer.add_widget(
+                    ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+                )
+            self.alreadyCheckNav = True
+
+        if self.alreadyCheck == False:
+            for i in self.PantryList: #prints all the items in user local list
+                self.ids.container.add_widget(
+                    SwipeItem_Pantry(text = i)
+                )
+            self.alreadyCheck = True
+
+    def remove_item(self,instance): #callback function to remove widgets, used for deleting items from the list
+        self.ids.container.remove_widget(instance)
+
+    def JSON_maker(self,food,date,quant):
+        #debug
+        print(food)  #returns the food name, will need to change the variable name of this
+        #debug
+        print("date in JSON maker: " + str(date))
+        
+        if date != None:
+            print(date) #debug
+
+        if date != None: #if the date is not empty then set the expires boolean to true
+            exp_bool = 1
+        else:
+            exp_bool = 0 #otherwise set it to false
+
+        if date != None: #2 different queries have to be called based on whether a date was given or not
+            App.db.exec(f"INSERT INTO Food(owner,name,expired,exp_date) VALUES('{App.user.username}', '{food}', '{exp_bool}', '{date}')")
+        else:
+            App.db.exec(f"INSERT INTO Food(owner,name,expired) VALUES('{App.user.username}', '{food}', '{exp_bool}')")
+
+        JSON = { #debug
+            "Owner" : App.user.username,
+            "Name" : food,
+            "Expires": exp_bool,
+            "Exp_date": date,
+            "Quantity": quant,
+            "Type": None
+            }
+
+        print(JSON) #debug
+
+    def got_date(self, the_date): #gets date from the calender
+        self.bufferDate = the_date
+        return(self.bufferDate)
+
+    def show_date_picker(self): #opens up the calender
+        date_dialog = MDDatePicker(callback=self.got_date)
+        date_dialog.open()
+
+    def add_pantry_item(self,instance):#Opens dialog box and prompts for additional information needed for the add to pantry functionality
+        
+        self.bufferDate = None #reset bufferdate back to null when dialog box opens
+        self.food_name = instance.text #this gets the title of the item clicked
+        self.pantry_item_instance = instance
+
+        #So looks like variables need to use self. to be able to use elsewhere
+        close_button = MDRectangleFlatButton(text = 'Close', on_release=self.close_dialog)
+        submit_button = MDRectangleFlatButton(text = 'Submit', on_release=self.submit_dialog)
+        self.dialog = MDDialog(
+            title = "Add item to Pantry?",
+            size_hint=(0.8,1),
+            type="custom",
+            content_cls = dialog_content(),
+            buttons=[submit_button,close_button],
+        )
+        self.dialog.open()
+        # open thingy that prompts for more info and then creates a food object which is then sent to the food handler
+
+
+    def close_dialog(self,instance): #closes the dialog box
+        self.dialog.dismiss()
+
+    def submit_dialog(self,instance):
+        #quant = self.dialog.content_cls.ids.quantity.text
+        
+        if App.sm.get_screen("window2").bufferDate: #if a date was selected assign it to a nicer variable name
+            date = App.sm.get_screen("window2").bufferDate
+        else: #else let it be empty
+            date = None
+        
+        if self.dialog.content_cls.ids.quantity.text: #If quantity was chosen assign it to a nicer variable name
+            quant = self.dialog.content_cls.ids.quantity.text
+        else: #else it defaults to 1
+            quant = 1
+
+
+
+        self.JSON_maker(self.food_name,date,quant) #send collected info to be sent to the database
+        #after submitting, remove the item and close the box
+        self.remove_item(self.pantry_item_instance) #removes the item from the list when button is pressed
+        self.dialog.dismiss()
+
+    def call_back(self,instance):#debug
+        if (instance.icon == 'delete'):
+            self.deletion()
+        else:
+            self.show_data(self)
+
+    def deletion(self):
+        for delete in CheckedItemsList:
+            self.alreadyCheck = False;
+            self.PantryList.remove(delete)
+            self.ids.container.clear_widgets()
+            if self.alreadyCheck == False:
+                for i in self.PantryList: #prints all the items in user local list
+                    self.ids.container.add_widget(
+                        SwipeItem(text = i)
+                    )
+            self.alreadyCheck = True
+        CheckedItemsList.clear()
+        print(*self.PantryList, sep='\n')
+
+    def show_data(self, obj):
+        close_button = MDRectangleFlatButton(
+            text = "Add",
+            pos_hint = {"center_x": 0.5, "center_y": 0.4},
+            on_press = self.close_dialog,
+            on_release = self.add_to_list
+        )
+        self.alreadyCheck = False
+
+        x_button = MDFlatButton(
+            text = "X",
+            pos_hint = {"center_x": 1.0, "center_y": 3.5},
+            on_press = self.close_dialog
+        )
+
+        self.foodItem = MDTextField(
+            hint_text = "Enter an item",
+            helper_text = "e.g. apples, bananas, orange, etc.",
+            helper_text_mode = "on_focus",
+            # icon_right_color = app.theme_cls.primary_color,
+            pos_hint = {"center_x": 0.5, "center_y": 0.5},
+            size_hint_x = None,
+            width = 250
+        )
+
+        self.dialog = MDDialog(
+            title = "Enter an item:",
+            size_hint = (0.7, 1),
+            buttons = [close_button, x_button]
+        )
+
+        self.dialog.add_widget(self.foodItem)
+
+        self.dialog.open()
+        self.alreadyCheck = True
+
+
+    def add_to_list(self, obj):
+        self.PantryList.append(self.foodItem.text)
+        self.ids.container.add_widget(
+            SwipeItem(text = self.foodItem.text)
+        )
 
 class Window1(Screen): # WELCOME WINDOW
     pass
@@ -332,6 +526,14 @@ class SwipeItem(MDCardSwipe):
     text = StringProperty()
     icon = StringProperty('android')
 
+
+class SwipeItem_Pantry(MDCardSwipe):
+    '''' Card with behavior '''
+    text = StringProperty()
+    icon = StringProperty('android')
+
+
+
 class ListItemWithCheckbox(OneLineAvatarIconListItem):
     '''' list item '''
     icon = StringProperty('android')
@@ -398,7 +600,7 @@ class App(MDApp):
     sm.add_widget(Pantry(name="pantry"))
     
     def build(self):
-        screen = Builder.load_file('test.kv')
+        screen = Builder.load_file('MainAppScreens.kv') #changed the name here 
         return screen
 
 App().run() #runs the app
